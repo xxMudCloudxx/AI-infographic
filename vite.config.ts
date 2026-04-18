c867d5d3e8d749aa983da0eaeb81ef98c21ce46f
xxMudCloudxx <xxbaizero@gmail.com>
2026-04-19 02:31:57 +0800

    ci：增加部署脚本

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/AI-infographic/",
   plugins: [react(), tailwindcss()],
   test: {
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
   },
-})
+});
